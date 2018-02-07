2f8ec017e9c40e61c7a97cdff2ff3c1ed8c08fd7
Jason Abbott <dev@trailimage.com>
2018-02-06 19:14:30 -0700

    circleci test

--- lib/logger.test.ts
+++ lib/logger.test.ts
@@ -4,23 +4,27 @@ const logMock = jest.fn();
 console.log = logMock;
 
 beforeAll(() => {
-   log.update({ color: false })
+   log.update({ color: false });
 });
 
 test('formats log message', () => {
-   expect(log.format(LogLevel.Error, "error message")).toMatchSnapshot();
-   expect(log.format(LogLevel.Warn, "warn message", { key: "value" })).toMatchSnapshot();
+   expect(log.format(LogLevel.Error, 'error message')).toMatchSnapshot();
+   expect(
+      log.format(LogLevel.Warn, 'warn message', { key: 'value' })
+   ).toMatchSnapshot();
    log.update({ readable: false });
-   expect(log.format(LogLevel.Info, "info message")).toMatchSnapshot();
+   expect(log.format(LogLevel.Info, 'info message')).toMatchSnapshot();
 });
 
 test('call', () => {
-   log.update({ readable: true })
+   log
+      .update({ readable: true })
       .debug('message', { data: [{ index: 1 }, { index: 2 }] });
 
-   expect(logMock).toHaveBeenCalledWith("[Debug] message data#0#index=1 data#1#index=2 level=1 message=message");
+   expect(logMock).toHaveBeenCalledWith(
+      '[Debug] message data#0#index=1 data#1#index=2 level=1 message=message'
+   );
 });
-
 
 test('export `log`', () => {
    expect(log).toBeDefined();
